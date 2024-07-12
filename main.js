ff754a3c18773b72873e182a3a93b430e0062cf4
Gennadiy99 <pitsportzp@gmail.com>
2024-07-12 23:54:12 +0200

    modal

---
+++ main.js
@@ -0,0 +1,23 @@
+
+function openModal() {
+  document.querySelector(".first-modal").style.display = "block";
+}
+document.querySelector(".design-flex__btn").addEventListener("click", openModal);
+
+function closеModal() {
+  document.querySelector(".first-modal").style.display = "none";
+}
+document.querySelector(".clouse__btn").addEventListener("click", closеModal);
+// кнопка и отправка текста из поля на почту
+let sendBtn = document.querySelector('.send__btn');
+let messageFild = document.querySelector(".first-modal-mesage");
+
+sendBtn.addEventListener('click', function() {
+  let message = messageFild.value;
+
+  let email = "login841@gmail.com";
+  let subject = "Вопрос по разработе";
+  let mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(message)}`;
+  window.location.href = mailtoLink;
+  // console.log(message);
+})
